294b6e8b7151f5c4040265eebbc0b2a06126e37e
ADRI22904 <adriana.agueromorales@ucr.ac.cr>
2026-03-02 17:26:15 -0600

    Update CODIGO.py

--- CODIGO.py
+++ CODIGO.py
@@ -198,7 +198,7 @@ if st.session_state.autenticado and st.session_state.nombre_sel == nombre_sel:
     actividad_sel = st.selectbox("Selecciona una actividad:", opciones_actividades)
 
     if st.button("Generar PDF"):
-        if nombre_sel.strip().lower() == "ARPYMES" and actividad_sel.strip().lower() == "sesión de trabajo con empresa":
+        if nombre_sel.strip().lower() == "arpymes" and actividad_sel.strip().lower() == "sesión de trabajo con empresa":
             registros = df_respuestas[
                 (df_respuestas["Seleccione el tipo de actividad que realizó"].str.strip().str.lower() == actividad_sel.strip().lower()) &
                 (df_respuestas["Indique el proyecto o unidad para el cuál realizó la tarea."].str.strip().str.lower() == "arpymes")
